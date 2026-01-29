46dc7df863a1135f9a5dd93680d361e39e8bc96c
DominicDev <s0l0estudi4r@gmail.com>
2026-01-29 15:46:33 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'NICKY JAM';
+export let nameSongs = [
+	"EL PERDÓN", /*1*/
+	"TRAVESURAS", /*2*/
+	"EL AMANTE", /*3*/
+	"HASTA EL AMANECER", /*4*/
+	"SI TÚ LA VES ", /*5*/
+	"X", /*6*/
+	"CÁSATE CONMIGO", /*7*/
+	"TE ROBARÉ", /*8*/
+	"SI TÚ NO ESTÁS ", /*9*/
+	"NO TE VAYAS", /*10*/
+	"POR EL MOMENTO", /*11*/
+	"CUANDO QUIERAS", /*12*/
+	"MIL LAGRIMAS", /*13*/
+	"PIENSAS EN MI", /*14*/
+	"VOY A BEBER", /*15*/
+	"JUEGOS PROHIBIDOS", /*16*/
+	"FAN DE TUS FOTOS", /*17*/
+	"WHINE UP", /*18*/
+	"MUÉVELO", /*19*/
+	"SIN FILTRO", /*20*/
+]
